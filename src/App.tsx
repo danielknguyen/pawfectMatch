6278a2e92cea7a4fcc3feb8cbe2d11f4467d8978
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-03 00:14:03 -0800

    added routes for both login and home page; include Navigation bar on every page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "pages/LoginPage";
+import { HomePage } from "pages/HomePage";
+import { Navbar } from "components/Navbar";
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar title="PawfectMatch" isLoggedIn={true} />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
