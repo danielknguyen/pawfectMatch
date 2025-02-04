18de650cb1b34ae53f466dea54eb1ab75f35abf3
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-03 16:13:07 -0800

    pass isAuthenticated down to navigation as prop.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from "pages/LoginPage";
 import { HomePage } from "pages/HomePage";
 import { Navbar } from "components/Navbar";
+import { selectIsAuthenticated } from "store/selectors/authSelectors";
+import { useSelector } from "react-redux";
 
 export const App = () => {
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  
+  console.log("isAuthenticated: ", isAuthenticated)
+
   return (
     <>
-      <Navbar title="PawfectMatch" isLoggedIn={true} />
+      <Navbar title="PawfectMatch" isLoggedIn={isAuthenticated} />
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
