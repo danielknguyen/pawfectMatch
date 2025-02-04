5fe543f06fe2a9a93444d2cfa869da59edc5526d
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-03 16:43:51 -0800

    added handle logging out method.

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,29 @@ import { HomePage } from "pages/HomePage";
 import { Navbar } from "components/Navbar";
 import { selectIsAuthenticated } from "store/selectors/authSelectors";
 import { useSelector } from "react-redux";
+import { authApi } from "services/modules/authApi";
+import { useAppDispatch } from "store/hooks/useAppDispatch";
+import { logout } from "store/actions/authActions";
+
+const authApiInstance = authApi();
 
 export const App = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated)
 
+  const dispatch = useAppDispatch();
+
+  const handleLoggingOut = async() => {
+    // TODO: handle if logout fails
+    await authApiInstance.logout();
+
+    dispatch(logout());
+
+    window.location.href = "/login";
+  }
+
   return (
     <>
-      <Navbar title="PawfectMatch" isLoggedIn={isAuthenticated} />
+      <Navbar title="PawfectMatch" isLoggedIn={isAuthenticated} logout={handleLoggingOut} />
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
