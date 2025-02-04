f4427284454a5664b2497a1aee4896970768fbc8
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-03 16:13:39 -0800

    Consume persisted store.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "store/store";
 import "./index.css";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
