ac5b58bee30d05d923183038ce1791c96bb56caa
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-03 16:09:12 -0800

    updated aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
     alias: {
       components: "/src/components",
       pages: "/src/pages",
-      redux: "/src/redux",
+      store: "/src/store",
+      services: "/src/services",
     },
   },
 });
