88d3d04ae5fdea2b9a689039f3d0cf3d526f87b9
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-04 23:46:50 -0800

    combine dogReducer.

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
 import { filterReducer } from "./filterReducer";
+import { dogReducer } from "./dogReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
   filters: filterReducer,
+  dogs: dogReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
