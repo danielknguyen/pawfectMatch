475b558fd716fb6d660b63e12410714251980c46
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 14:09:48 -0800

    handle checking authentication status and handle potential errors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from "pages/LoginPage";
 import { SearchPage } from "pages/SearchPage";
@@ -5,28 +6,57 @@ import { Navbar } from "components/Navbar";
 import { selectIsAuthenticated } from "store/selectors/authSelectors";
 import { useSelector } from "react-redux";
 import { authApi } from "services/modules/authApi";
+import { searchApi } from "services/modules/searchApi";
 import { useAppDispatch } from "store/hooks/useAppDispatch";
 import { logout } from "store/actions/authActions";
+import { handleAuthError } from "utils/handleAuthError";
 
+const searchApiInstance = searchApi();
 const authApiInstance = authApi();
 
 export const App = () => {
-  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const isAuthenticated = useSelector(selectIsAuthenticated);
 
   const dispatch = useAppDispatch();
 
-  const handleLoggingOut = async() => {
+  const handleLoggingOut = async () => {
     // TODO: handle if logout fails
     await authApiInstance.logout();
 
     dispatch(logout());
 
     window.location.href = "/login";
-  }
+  };
+
+  /**
+   * useEffect hook to check authentication status and handle potential errors.
+   * If the user is authenticated, it performs a search request to verify session validity.
+   * If the request fails, it handles the authentication error by logging the user out.
+   */
+  useEffect(() => {
+    if (isAuthenticated) {
+      (async function checkAuth() {
+        try {
+          const response = await searchApiInstance.search({});
+
+          if (!response.ok) {
+            const error = await response.json();
+            handleAuthError(error, handleLoggingOut);
+          }
+        } catch (error) {
+          handleAuthError(error, handleLoggingOut);
+        }
+      })();
+    }
+  }, [isAuthenticated]);
 
   return (
     <>
-      <Navbar title="PawfectMatch" isLoggedIn={isAuthenticated} logout={handleLoggingOut} />
+      <Navbar
+        title="PawfectMatch"
+        isLoggedIn={isAuthenticated}
+        logout={handleLoggingOut}
+      />
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
