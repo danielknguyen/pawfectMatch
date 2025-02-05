d2d20e24c9988e3f3514d74d3e53198d090984b1
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-04 23:02:53 -0800

    created module for all search endpoints.

---
+++ src/services/modules/searchApi.ts
@@ -0,0 +1,40 @@
+import { Api } from "services/api";
+import { FiltersState } from "store/types/filterTypes";
+
+const apiInstance = new Api();
+
+export const searchApi = () => ({
+  search: async ({
+    breeds = [],
+    zipCodes = [],
+    ageMin,
+    ageMax,
+    size = 25,
+    from = 0,
+    sort = "breed:asc",
+  }: FiltersState) => {
+    const queryParams = new URLSearchParams();
+
+    if (breeds.length) queryParams.append("breeds", breeds.join(","));
+    if (zipCodes.length) queryParams.append("zipCodes", zipCodes.join(","));
+    if (ageMin !== undefined) queryParams.append("ageMin", String(ageMin));
+    if (ageMax !== undefined) queryParams.append("ageMax", String(ageMax));
+    if (size) queryParams.append("size", String(size));
+    if (from) queryParams.append("from", String(from));
+    if (sort) queryParams.append("sort", sort);
+
+    return apiInstance.get(`/dogs/search?${queryParams.toString()}`);
+  },
+
+  fetchDogsByIds: async (ids: string[]) => {
+    return apiInstance.post("/dogs", ids);
+  },
+
+  fetchDogsByBreeds: async () => {
+    return apiInstance.get("/dogs/breeds");
+  },
+
+  matchDogs: async (ids: string[]) => {
+    return apiInstance.post("/dogs/match", ids);
+  },
+});
