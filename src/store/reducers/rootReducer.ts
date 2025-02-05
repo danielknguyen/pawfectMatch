4e021b57a78a0003d48c388b19c975b563dc09f5
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-04 22:45:07 -0800

    renamed and combined filterReducer.

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
+import { filterReducer } from "./filterReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  filters: filterReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
