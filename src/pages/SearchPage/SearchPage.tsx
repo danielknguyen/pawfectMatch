4f13e08ca8ab21628c2ff6ad0934eef1316044e6
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 22:39:21 -0800

    logic to render favorites sidebar.

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Pagination } from "components/Pagination";
+import { FavoriteDogsSidebar } from "pages/SearchPage/components/FavoriteDogsSidebar";
 import { useAppDispatch } from "store/hooks/useAppDispatch";
 import { selectIsAuthenticated } from "store/selectors/authSelectors";
 import { selectFilters } from "store/selectors/filterSelectors";
 import {
   selectAllDogs,
+  selectFavorites,
   selectSearchResults,
 } from "store/selectors/dogSelectors";
 import {
@@ -15,10 +17,16 @@ import {
 } from "store/actions/dogActions";
 import { Filters, DogProfilesGrid } from "pages/SearchPage/components";
 import { searchApi } from "services/modules/searchApi";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const mainStyle = {
   flexGrow: 1,
   p: 3,
+};
+
+const headerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
 };
 
 const searchApiInstance = searchApi();
@@ -35,6 +43,9 @@ export const SearchPage = () => {
   const dogs = useSelector(selectAllDogs);
   const searchResults = useSelector(selectSearchResults);
   const filters = useSelector(selectFilters);
+  const favorites = useSelector(selectFavorites);
+
+  const [isFavoritesOpen, setIsFavoritesOpen] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -72,14 +83,36 @@ export const SearchPage = () => {
     dispatch(fetchDogsSuccess(dogSearchResults));
   };
 
+  const handleOpeningFavorites = () => {
+    setIsFavoritesOpen(true);
+  };
+
+  const handleClosingFavorites = () => {
+    setIsFavoritesOpen(false);
+  };
+
+  const hasFavorites = Object.keys(favorites).length > 0;
+
   return (
     <Box component="main" sx={mainStyle}>
-      <Filters />
+      <Box sx={headerStyle}>
+        <Filters />
+        <Button
+          onClick={handleOpeningFavorites}
+          color={hasFavorites ? "primary" : "inherit"}
+        >
+          <FavoriteIcon /> Favorites ({Object.keys(favorites).length})
+        </Button>
+      </Box>
       <DogProfilesGrid dogs={dogs} />
       <Pagination
         total={searchResults.total}
         size={filters.size}
         onChange={handlePageChange}
+      />
+      <FavoriteDogsSidebar
+        open={isFavoritesOpen}
+        onClose={handleClosingFavorites}
       />
     </Box>
   );
