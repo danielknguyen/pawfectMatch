c93690838911a693a6e00bbe1b99119610ff00f8
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 20:26:32 -0800

    add selector to fetch filters state.

--- src/store/selectors/filterSelectors.ts
+++ src/store/selectors/filterSelectors.ts
@@ -1,9 +1,7 @@
 import { RootState } from "store/reducers/rootReducer";
 
 export const selectBreeds = (state: RootState) => state.filters.breeds;
-export const selectZipCodes = (state: RootState) => state.filters.zipCodes;
-export const selectAgeMin = (state: RootState) => state.filters.ageMin;
-export const selectAgeMax = (state: RootState) => state.filters.ageMax;
 export const selectSize = (state: RootState) => state.filters.size;
 export const selectFrom = (state: RootState) => state.filters.from;
 export const selectSort = (state: RootState) => state.filters.sort;
+export const selectFilters = (state: RootState) => state.filters;
