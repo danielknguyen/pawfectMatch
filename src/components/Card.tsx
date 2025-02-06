bb3f8f8586199cab663457e9f53f91a9c1736bf3
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 21:20:33 -0800

    removed card action area.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,6 @@ import { Card as MuiCard } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
 import CardActions from "@mui/material/CardActions";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import IconButton from "@mui/material/IconButton";
@@ -28,26 +27,24 @@ export const Card = ({
 }: CardProps) => {
   return (
     <MuiCard sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia component="img" height="140" image={img} alt={breed} />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography
-            variant="body2"
-            sx={{
-              color: "text.secondary",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <span>Age: {age}</span>
-            <span>Zip Code: {zipCode}</span>
-            <span>Breed: {breed}</span>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia component="img" height="140" image={img} alt={breed} />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            color: "text.secondary",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <span>Age: {age}</span>
+          <span>Zip Code: {zipCode}</span>
+          <span>Breed: {breed}</span>
+        </Typography>
+      </CardContent>
       <CardActions>
         <IconButton onClick={onClick} color={selected ? "primary" : "default"}>
           <FavoriteIcon />
