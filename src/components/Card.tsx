cabe73a884d10864e9d65699cd233824a5061c0a
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 20:27:48 -0800

    reusable Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,56 @@
+import { Card as MuiCard } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+
+interface CardProps {
+  img: string;
+  name: string;
+  age: number;
+  zipCode: string;
+  breed: string;
+  onClick: () => void;
+}
+
+export const Card = ({
+  img,
+  name,
+  age,
+  zipCode,
+  breed,
+  onClick,
+}: CardProps) => {
+  return (
+    <MuiCard sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={img} alt={breed} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              color: "text.secondary",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <span>Age: {age}</span>
+            <span>Zip Code: {zipCode}</span>
+            <span>Breed: {breed}</span>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <IconButton onClick={onClick}>
+          <FavoriteIcon />
+        </IconButton>
+      </CardActions>
+    </MuiCard>
+  );
+};
