68b58c67f02ef78f9ec2d227172840b1db785493
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 21:11:48 -0800

    added paw icon to title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import PetsIcon from "@mui/icons-material/Pets";
+import { Icon } from "@mui/material";
 
 const boxStyle = {
   flexGrow: 1,
@@ -18,12 +20,17 @@ const linkStyle = {
   color: "inherit",
 };
 
+const iconStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: 1,
+};
+
 interface NavbarProps {
   title: string;
   isLoggedIn: boolean;
   logout: () => void;
 }
-
 
 /**
  * A component that renders a navigation bar with a title and a button
@@ -39,10 +46,13 @@ export const Navbar = ({ title, isLoggedIn, logout }: NavbarProps) => {
     <Box sx={boxStyle}>
       <AppBar position="static" color="inherit">
         <Toolbar>
-          <Typography variant="h4" component="div" sx={TypographyStyle}>
-            <a href="/" style={linkStyle}>
-              {title}
-            </a>
+          <Typography variant="h5" component="div" sx={TypographyStyle}>
+            <Box sx={iconStyle}>
+              <PetsIcon color="primary" fontSize="large" />
+              <a href="/" style={linkStyle}>
+                {title}
+              </a>
+            </Box>
           </Typography>
           {!isLoggedIn && (
             <Button color="inherit" href="/login">
