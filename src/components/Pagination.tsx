3a04e1a673add701b5315b36302096b8e18e88ec
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 20:27:59 -0800

    reusable Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+import { Box, Pagination as MuiPagination } from "@mui/material";
+
+const paginationStyles = {
+  mt: 2,
+  mb: 2,
+  m: 3,
+};
+
+interface PaginationProps {
+  total: number;
+  size?: number;
+  onChange: (event: React.ChangeEvent<unknown>, page: number) => void;
+}
+
+export const Pagination = ({ total, size = 25, onChange }: PaginationProps) => {
+  return total > 0 ? (
+    <Box display="flex" justifyContent="center">
+      <MuiPagination
+        count={Math.ceil(total / size)}
+        onChange={onChange}
+        sx={paginationStyles}
+      />
+    </Box>
+  ) : (
+    <></>
+  );
+};
