b7dbe5e864efea8636f4c9a8cdabadbfa236d519
Daniel Nguyen <danielknguyen@danielknguyen-MacBook-Pro.local>
2025-02-05 20:24:58 -0800

    added search functionality.

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,9 +1,28 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Box } from "@mui/material";
+import { Pagination } from "components/Pagination";
+import { useAppDispatch } from "store/hooks/useAppDispatch";
 import { selectIsAuthenticated } from "store/selectors/authSelectors";
+import { selectFilters } from "store/selectors/filterSelectors";
+import {
+  selectAllDogs,
+  selectSearchResults,
+} from "store/selectors/dogSelectors";
+import {
+  fetchDogsSuccess,
+  fetchDogsByIdSuccess,
+} from "store/actions/dogActions";
+import { Filters, DogProfilesGrid } from "pages/SearchPage/components";
 import { searchApi } from "services/modules/searchApi";
 
-export const searchApiInstance = searchApi();
+const mainStyle = {
+  flexGrow: 1,
+  p: 3,
+  height: "100vh",
+};
+
+const searchApiInstance = searchApi();
 
 export const SearchPage = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
@@ -12,19 +31,57 @@ export const SearchPage = () => {
     window.location.href = "/login";
   }
 
-  useEffect(() => {
-    (() => {
-      (async () => {
-        try {
-          const response = await searchApiInstance.search({});
-          const data = await response.json();
-          console.log("dogs", data);
-        } catch (error) {
-          console.error(error);
-        }
-      })();
-    })();
-  });
+  const dispatch = useAppDispatch();
 
-  return <></>;
+  const dogs = useSelector(selectAllDogs);
+  const searchResults = useSelector(selectSearchResults);
+  const filters = useSelector(selectFilters);
+
+  useEffect(() => {
+    (async () => {
+      const dogSearch = await searchApiInstance.search(filters);
+
+      const dogSearchResults = await dogSearch.json();
+
+      dispatch(fetchDogsSuccess({ ...dogSearchResults }));
+    })();
+  }, [filters]);
+
+  useEffect(() => {
+    (async () => {
+      const dogs = await searchApiInstance.fetchDogsByIds(
+        searchResults.resultIds
+      );
+
+      const dogResults = await dogs.json();
+
+      dispatch(fetchDogsByIdSuccess(dogResults));
+    })();
+  }, [searchResults]);
+
+  const handlePageChange = async (
+    _event: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    const dogSearch = await searchApiInstance.search({
+      ...filters,
+      from: (page - 1) * (filters?.size ?? 25),
+    });
+
+    const dogSearchResults = await dogSearch.json();
+
+    dispatch(fetchDogsSuccess(dogSearchResults));
+  };
+
+  return (
+    <Box component="main" sx={mainStyle}>
+      <Filters />
+      <DogProfilesGrid dogs={dogs} />
+      <Pagination
+        total={searchResults.total}
+        size={filters.size}
+        onChange={handlePageChange}
+      />
+    </Box>
+  );
 };
